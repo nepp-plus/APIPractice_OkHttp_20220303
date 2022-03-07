7a773b5134d2618e48070d84a67e25e25bed816a
nepp.plus <nepp.plus@gmail.com>
2022-03-07 09:48:36 +0900

    BaseActivity 상속 처리  - 모든 액티비티에 이 세팅도 진행 예정

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,15 +2,26 @@ package com.neppplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
