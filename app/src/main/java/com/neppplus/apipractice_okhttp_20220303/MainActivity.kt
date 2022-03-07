85b1d5a2dd4eb01efcd7df4c5f2c0d292f8132b3
nepp.plus <nepp.plus@gmail.com>
2022-03-07 15:48:01 +0900

    연습문제 마무리  - 토큰을 첨부해서, 토큰에 맞는 내 정보를 받아오기

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -25,7 +26,24 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
 //        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.get
+        ServerUtil.getRequestMyInfo(mContext,  object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+
+                val userObj = dataObj.getJSONObject("user")
+
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread {
+
+                    binding.txtLoginUserNickname.text = nickname
+
+                }
+
+            }
+
+        })
 
     }
 }
