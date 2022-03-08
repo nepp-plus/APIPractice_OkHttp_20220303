1421f02b0e7c24d042c4a2ae934849aeafd22642
nepp.plus <nepp.plus@gmail.com>
2022-03-08 12:20:59 +0900

    파싱 완료된, 토론주제의 > 진영목록을 UI 반영

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -42,6 +42,10 @@ class ViewTopicDetailActivity : BaseActivity() {
         binding.txtTitle.text = mTopicData.title
         Glide.with(mContext).load(mTopicData.imageURL).into(binding.imgTopicBackground)
 
+//        1번진영 제목, 2번진영 제목
+        binding.txtSide1.text = mTopicData.sideList[0].title
+        binding.txtSide2.text = mTopicData.sideList[1].title
+
     }
 
     fun getTopicDetailFromServer() {
